7a77c5d67edc6ea3ce077664d2ca8fbd07d57906
Yassir aittata <yassir.aittata@gmail.com>
2024-02-02 21:59:39 +0000

    setup the microservice todo

--- apps/todo/src/main.ts
+++ apps/todo/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 import { TodoModule } from './todo.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(TodoModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    TodoModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'todo',
+        protoPath: join(__dirname, '../todo.proto'),
+      },
+    },
+  );
+
+  await app.listen();
 }
 bootstrap();
